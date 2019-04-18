ff1100decc7e99ccf37806153bee91bd3b288fd8
brunomb91 <bruno.mb.91@gmail.com>
2019-04-18 02:48:37 -0300

    Adição de tratamento de erros

--- RSA.py
+++ RSA.py
@@ -3,8 +3,8 @@ import random
 class RSA:
     """RSA cryptography implementation."""
     def __init__(self):
-        # __p = random.choice(self.prime_numbers_until_n())
-	# __q = random.choice(self.prime_numbers_until_n())
+        # __p = 0
+	# __q = 0
         self.__p = 97
         self.__q = 53
         self.n = 0
@@ -13,7 +13,26 @@ class RSA:
         self.cod = []
         self.decod = []
          
-    def setup(self):
+    def setup(self, chave_publica = (), chave_privada = ()):
+        if chave_publica != () and chave_privada != ():
+            self.__chave_encrypt = list(self.__chave_encrypt)
+            self.__chave_decrypt = list(self.__chave_decrypt)
+            chave_publica = list(chave_publica)
+            chave_privada = list(chave_privada)
+            
+            self.__chave_encrypt = chave_publica
+            self.__chave_decrypt = chave_privada
+            
+            self.__chave_encrypt = tuple(self.__chave_encrypt)
+            self.__chave_decrypt = tuple(self.__chave_decrypt)
+            chave_publica = tuple(chave_publica)
+            chave_privada = tuple(chave_privada)
+            
+            return chave_publica
+	
+	# __p = random.choice(self.prime_numbers_until_n())
+	# __q = random.choice(self.prime_numbers_until_n())
+
         self.n = self.__p * self.__q
         phi = lambda n: n - 1
         # n1 = random.randint(1, 11)
@@ -35,7 +54,14 @@ class RSA:
         self.__chave_decrypt.append(self.n)
         self.__chave_decrypt.append(self.decrypt_value(c, n1))
         self.__chave_decrypt = tuple(self.__chave_decrypt)
-        	
+        
+        chave_publica = list(chave_publica)
+        chave_publica = self.__chave_encrypt
+        chave_publica = tuple(chave_publica)
+        
+        return chave_publica
+        
+
     def prime_numbers_until_n(self, n=1000):
         l = []
         for i in range(1,n):
@@ -67,14 +93,12 @@ class RSA:
 	
         return d    
 
-    def encrypt(self, msg):
+    def encrypt_msg(self, msg, chave_publica = ()):
         pre_cod = [str(ord(x) + 100) for x in msg]
         pre_cod = ''.join(pre_cod)
-        l = []
-        
-        # inicio = 0
-        # fim = inicio + random.randint(2, 4)
+        l = []        
         tam = len(pre_cod) 
+        cod = []
         
         while len(l) != 10:
             l = []
@@ -85,12 +109,40 @@ class RSA:
                 inicio = fim
                 fim = inicio + random.randint(2, 4)
         
-        cod = [((int(i)**self.__chave_encrypt[1]) % self.__chave_encrypt[0]) for i in l]    
+        if chave_publica != ():
+            cod = [((int(i)**chave_publica[1]) % chave_publica[0]) for i in l]
+        
+        else:
+            cod = [((int(i)**self.__chave_encrypt[1]) % self.__chave_encrypt[0]) for i in l]
+        
         cod = [str(i) for i in cod]
         cod = '#'.join(cod)
+        
         return cod
         
+    def encrypt(self, msg, chave_publica = ()): 
+        cod = self.encrypt_msg(msg, chave_publica)
+         
+        # if self.error_treatment(cod) == False:
+        #     while self.error_treatment(msg) != True:
+        #         cod = self.encrypt_msg(msg)	    
+        
+        # cod = '#'.join(cod)
 	
+        return cod
+        
+
+    def error_treatment(self, l):
+        if l == []:
+            return False
+        for j in l:
+            if int(j) <= 165 or int(j) >= 190:
+                if int(j) == 132:
+                    continue
+                else:    
+                    return False
+        return True
+        
     def decrypt(self, msg):
         msg = msg.split('#')
         l1 = []
@@ -108,9 +160,12 @@ class RSA:
             self.decod.append(l1[inicio:fim])
             inicio = fim
             fim = inicio + 3
-	
-        self.decod = [chr(int(i) - 100) for i in self.decod if int(i) <= 190 or int(i) >= 132]
-        self.decod = ''.join(self.decod)
+        
+        try:
+            self.decod = [chr(int(i) - 100) for i in self.decod]
+            self.decod = ''.join(self.decod)
+        except: 
+            return 'Invalid'
         
         return self.decod
 
